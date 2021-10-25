f431f84e7696d3d48ed12a5e2b74af1f24b736a5
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-10-25 21:33:43 +0300

    Structure for Html, plain JS to add and remove books

---
+++ books.js
@@ -0,0 +1,55 @@
+const placeToAddBook = document.querySelector('.bookList');
+const readTitle = document.querySelector('#title');
+const readAuthor = document.querySelector('#author');
+
+// Array for storing a books
+let arrayBooks = [];
+
+function setFromLocalStorage() {
+  arrayBooks = JSON.parse(localStorage.getItem('books'));
+}
+
+function setToLocalStorage(books) {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function addBook() {
+  const bookToStore = {
+    title: readTitle.value,
+    author: readAuthor.value,
+  }
+  arrayBooks.push(bookToStore);
+  setToLocalStorage(arrayBooks);
+}
+
+function addToPage(book) {
+    placeToAddBook.innerHTML += `
+    <div class="myDiv">
+      <h4 class="title">"${book.title}" By ${book.author}</h4>
+      <button class="remove">Remove</button>
+    </div>`;
+}
+
+if(localStorage.getItem('books')) {  
+  setFromLocalStorage();
+  arrayBooks.forEach((book) => {
+    addToPage(book);
+  });
+}
+
+function removeElement(index) {  
+  arrayBooks.splice(index, 1);
+  setToLocalStorage(arrayBooks);
+  window.location.reload(false);
+}
+
+// Add book button and event
+const addButton = document.querySelector('#addBook');
+addButton.addEventListener('click', addBook);
+
+// Remove book button and event
+const allRemoveBtn = document.querySelectorAll('.remove');
+removeButtons = Array.from(allRemoveBtn);
+removeButtons.forEach((book) =>book.addEventListener('click', () => {
+  removeElement(removeButtons.indexOf(book));
+}));
